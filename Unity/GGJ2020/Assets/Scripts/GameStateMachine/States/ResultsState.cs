43138776abf2d610bed68b0f4a4dcc1fb76c1e7a
Kev Adsett <kev.adsett@gmail.com>
2020-02-01 15:29:06 +0000

    Add Scene management to running and results states.

--- Unity/GGJ2020/Assets/Scripts/GameStateMachine/States/ResultsState.cs
+++ Unity/GGJ2020/Assets/Scripts/GameStateMachine/States/ResultsState.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 public class ResultsState : AbstractState
 {
     public ResultsState(GameStateMachine InStateMachine, GameObject InStateUI) : base(InStateMachine, InStateUI)
     {
+    }
+
+    override public void OnExit()
+    {
+        base.OnExit();
+        SceneManager.UnloadSceneAsync("Running");
     }
 }
--- Unity/GGJ2020/Assets/Scripts/GameStateMachine/States/RunningState.cs
+++ Unity/GGJ2020/Assets/Scripts/GameStateMachine/States/RunningState.cs
@@ -1,7 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 public class RunningState : AbstractState
 {
     public RunningState(GameStateMachine InStateMachine, GameObject InStateUI) : base(InStateMachine, InStateUI)
     {
+    }
+
+    override public void OnEnter()
+    {
+        base.OnEnter();
+        SceneManager.LoadScene("Running", LoadSceneMode.Additive);
     }
 }
