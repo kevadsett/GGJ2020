65b2415ebf6d1a45caf1b52dda20347e0f2f7108
Ege <hsgngr@gmail.com>
2020-02-01 16:19:43 +0000

    very weird bug

--- Unity/GGJ2020/Assets/junkInteraction.cs
+++ Unity/GGJ2020/Assets/junkInteraction.cs
@@ -6,28 +6,30 @@ public class junkInteraction : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D rb;
-    public Transform displayPoint;
 
-    public GameObject currentObj = null;
+    public GameObject currentObj;
+
+    public bool isCarrying = false;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentObj.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
     public void OnTriggerEnter2D(Collider2D other)
     {
       
         if (other.tag == "Junk") {
-
-            currentObj = other.gameObject;
-
-            Instantiate(currentObj, this.transform);
+            Debug.Log("entered");
+            isCarrying = true;
+            currentObj.SetActive(isCarrying);
+            currentObj.GetComponent<JunkBehaviour>().JunkType = other.GetComponent<JunkBehaviour>().JunkType;
             Destroy(other.gameObject);
         }
         
