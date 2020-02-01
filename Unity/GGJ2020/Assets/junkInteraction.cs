4194887a0953f192237a6c9ddbb20775196b88c6
Kev Adsett <kev.adsett@gmail.com>
2020-02-01 18:19:09 +0000

    Pick junk up, put it in machines.

--- Unity/GGJ2020/Assets/junkInteraction.cs
+++ Unity/GGJ2020/Assets/junkInteraction.cs
@@ -1,40 +1,47 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
+[RequireComponent(typeof(PlayerId))]
 public class junkInteraction : MonoBehaviour
 {
     // Start is called before the first frame update
     Rigidbody2D rb;
 
-    public GameObject currentObj;
+    public JunkBehaviour MyJunk;
 
     public bool isCarrying = false;
+
+    private PlayerId MyId;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        currentObj.SetActive(false);
-
+        MyJunk.gameObject.SetActive(false);
+        MyJunk.GetComponent<Rigidbody2D>().Sleep();
+        MyId = GetComponent<PlayerId>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     public void OnTriggerEnter2D(Collider2D other)
     {
-      
-        if (other.tag == "Junk") {
-           
+        switch (other.tag)
+        {
+        case "Junk":
             isCarrying = true;
-            currentObj.SetActive(isCarrying);
-            currentObj.GetComponent<JunkBehaviour>().SetJunkType(other.GetComponent<JunkBehaviour>().JunkType);
-          Debug.Log("currentObject type: " + currentObj.GetComponent<JunkBehaviour>().JunkType + " other: " + other.GetComponent<JunkBehaviour>().JunkType);
-            
-            
+            JunkBehaviour TheirJunk = other.GetComponent<JunkBehaviour>();
+            MyJunk.SetJunkType(TheirJunk.JunkType);
             Destroy(other.gameObject);
+            break;
+        case "JunkMachine":
+            if (isCarrying == false)
+            {
+                break;
+            }
+            JunkMachine JunkMachine = other.GetComponent<JunkMachine>();
+            if (JunkMachine.TrySlotJunk(MyJunk.JunkType, MyId.Id))
+            {
+                isCarrying = false;
+            }
+            break;
         }
+        MyJunk.gameObject.SetActive(isCarrying);
         
     }
 }
