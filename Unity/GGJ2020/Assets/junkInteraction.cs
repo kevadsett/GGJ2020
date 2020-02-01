d0da9fb0242f009589193356492a39809b39eb81
Ege <hsgngr@gmail.com>
2020-02-01 15:58:51 +0000

    12

--- Unity/GGJ2020/Assets/junkInteraction.cs
+++ Unity/GGJ2020/Assets/junkInteraction.cs
@@ -12,37 +12,24 @@ public class junkInteraction : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void OnTriggerEnter2D(Collider2D other)
     {
+      
         if (other.tag == "Junk") {
-            if (currentObj != null) {
-                
-            GameObject oldJunk= Instantiate(currentObj, displayPoint.position, transform.rotation, this.transform);
-                oldJunk.GetComponent<Rigidbody2D>().AddRelativeForce(Vector2.up * 100f);
-            }
+
             currentObj = other.gameObject;
-            
-            Instantiate(currentObj.transform.GetChild(0), displayPoint.position, transform.rotation,this.transform);
 
+            Instantiate(currentObj, this.transform);
+            Destroy(other.gameObject);
         }
-    }
-    public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if(collision.gameObject.tag == "Player")
-        {
-
-        }
-    }
-
-    private void DetachJunk() {
-
+        
     }
 }
