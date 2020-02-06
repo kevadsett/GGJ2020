3e0889f097d4f569fcf062cdb168421a190bf00f
gav <notoriousknave@gmail.com>
2020-02-06 21:54:08 +0000

    added a happy little line to show you where to take your stuff

---
+++ Unity/GGJ2020/Assets/Scripts/Character/FaceObjective.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FaceObjective : MonoBehaviour
+{
+    public Transform objTForm;
+    
+    void Update()
+    {
+        transform.up = (objTForm.position - transform.position).normalized;
+    }
+}
